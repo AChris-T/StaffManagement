50137dc4e7e98df6aab35ffcc9eae61bf192d6f5
Akinfenwa Taiwo Christopher <101477417+AChris-T@users.noreply.github.com>
2024-12-04 21:38:46 +0100

    feat:authentication

--- ClientSide/src/pages/index.js
+++ ClientSide/src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as Login } from './Login';
-export { default as Register } from './register';
+export { default as Register } from './Register';
 export { default as Home } from './Home';
 export { default as Payment } from './Payment';
 export { default as Shop } from './Shop';
